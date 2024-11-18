1349990fd15c3cf135cec99b0aa5eb429e4ffd24
Lucas Graeff Buhl-Nielsen <lucasbnielsen@hotmail.com>
2024-11-18 16:01:27 +0000

    regular socket integer key

--- redirect.bpf.c
+++ redirect.bpf.c
@@ -7,8 +7,8 @@
 #define LOCALHOST_IPV4 16777343
 
 struct {
-    __uint(type, BPF_MAP_TYPE_SOCKHASH);
-    __uint(max_entries, 65535);
+    __uint(type, BPF_MAP_TYPE_SOCKMAP);
+    __uint(max_entries, 2);
     __type(key, int);
     __type(value, int);
 } sock_map SEC(".maps");
@@ -21,9 +21,14 @@ int sk_msg_prog(struct sk_msg_md *msg)
     if(msg->remote_ip4 != LOCALHOST_IPV4 || msg->local_ip4!= LOCALHOST_IPV4)
         return SK_PASS;
 
-    int key = (bpf_htonl(msg->local_port) * 1000) + msg->remote_port;
+    int key;
+    if (bpf_htonl(msg->remote_port) == 3000) {
+        key = 1;
+    } else {
+        key = 0;
+    }
 
-    return bpf_msg_redirect_hash(msg, &sock_map, &key, BPF_F_INGRESS);
+    return bpf_msg_redirect_map(msg, &sock_map, key, BPF_F_INGRESS);
 }
 
 SEC("sockops")
@@ -39,9 +44,21 @@ int sockops_prog(struct bpf_sock_ops *skops){
         return BPF_OK;
     }
 
-    int key = (skops->remote_port * 1000) + bpf_htonl(skops->local_port);
+    if (!skops->sk)
+        return SK_PASS;
 
-    bpf_sock_hash_update(skops, &sock_map, &key, BPF_NOEXIST);
+    switch (skops->op) {
+        case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
+            if (bpf_sock_map_update(skops, &sock_map, &(__u32){ 0 }, BPF_ANY) != 0) {
+                bpf_printk("Failed to update active mapping\n");
+            }
+            break;
+        case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
+            if (bpf_sock_map_update(skops, &sock_map, &(__u32){ 1 }, BPF_ANY) != 0) {
+                bpf_printk("Failed to update passive mapping\n");
+            }
+            break;
+    }
 
     return BPF_OK;
 }
